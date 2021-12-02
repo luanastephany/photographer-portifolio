ee95657e004e2f98c4e36ab07f9e4bcc220e2b4c
Luana Stephany <ls.luanastephany@gmail.com>
2021-12-02 15:36:09 -0300

    criando hero

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Header from "../../components/Header";
+import styles from "./styles.module.scss";
 
 const Home = () => {
   return (
     <div>
       <Header />
+
+      <div className={styles.hero}>
+        <p className={styles.subtitle}>We are creative</p>
+        <h1 className={styles.title}>Photographers</h1>
+      </div>
     </div>
   );
 };
